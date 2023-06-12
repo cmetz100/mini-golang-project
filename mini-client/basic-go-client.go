9ce9aa0635b2fa9355b73fa8f0e2fdf8eaa2c71a
Caleb Metz <caleb.metz@datadoghq.com>
2023-06-12 16:37:05 -0400

    resolved pr issues

--- mini-client/basic-go-client.go
+++ mini-client/basic-go-client.go
@@ -1,97 +1,120 @@
 package main
 
 import (
-	"fmt"
-    "io/ioutil"
-    "net/http"
-    "time"
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
-	"github.com/DataDog/datadog-go/statsd"
+	"net/http"
 	"strconv"
+	"time"
+
+	"github.com/DataDog/datadog-go/statsd"
 )
 
-func addTask (id int, t string, d string, cb bool){
+const endpointURL string = "http://127.0.0.1:9000"
+
+func addTask(id int, t string, d string, cb bool) {
 	c := http.Client{Timeout: time.Duration(1) * time.Second}
-	values := map[string]interface{}{"id": id, "title": t, "description" : d, "completed": cb}
-	jsonValue, _ := json.Marshal(values)
-	resp, err := c.Post("http://127.0.0.1:9000/tasks/add", "application/json", bytes.NewBuffer(jsonValue))
+	values := map[string]interface{}{"id": id, "title": t, "description": d, "completed": cb}
+	jsonValue, err := json.Marshal(values)
 	if err != nil {
-        log.Fatal(err)
-    }
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Printf("Body : %s", body)
+		log.Panic(err)
+	}
+	resp, err := c.Post(endpointURL+"/tasks/add", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Panicln(err)
+	}
+	if resp.StatusCode != http.StatusCreated {
+		log.Panicln("invalid status code: ", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("Body : %s", body)
 }
 
-func completeTask (id int){
+func completeTask(id int) {
 	values := map[string]interface{}{"id": id}
 	jsonValue, _ := json.Marshal(values)
 	c := http.Client{Timeout: time.Duration(1) * time.Second}
-	req,err := http.NewRequest(http.MethodPatch,"http://127.0.0.1:9000/tasks/complete",bytes.NewBuffer(jsonValue))
-    if err != nil {
-        log.Fatal(err)
-    }
+	req, err := http.NewRequest(http.MethodPatch, endpointURL+"/tasks/complete", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Panic(err)
+	}
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := c.Do(req)
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Panic(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Panicln("invalid status code: ", resp.StatusCode)
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Println(string(body))
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println(string(body))
 
 }
 
-func getTasks(showCompleted bool){
-	url := "http://127.0.0.1:9000/tasks?showCompleted=" +strconv.FormatBool(showCompleted)
+func getTasks(showCompleted bool) {
+	url := endpointURL + "/tasks?showCompleted=" + strconv.FormatBool(showCompleted)
 	resp, err := http.Get(url)
 	if err != nil {
-	   log.Fatalln(err)
+		log.Panic(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Panicln("invalid status code: ", resp.StatusCode)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-      log.Fatalln(err)
-    }
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panic(err)
+	}
 	log.Println(string(body))
 }
 
-
 func newClient() *statsd.Client {
 	statsd, err := statsd.New("127.0.0.1:8125")
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Panic(err)
+	}
 	return statsd
 }
 
-func main(){
-	var client *statsd.Client =newClient()
+func main() {
+	var client *statsd.Client = newClient()
 
 	for {
 		for i := 1; i < 10; i++ {
 			title := "task #" + strconv.Itoa(i)
-	
+
 			//collect data on add tasks
 			start := time.Now()
-			addTask(i,title,"boo1",false)
+			addTask(i, title, "boo1", false)
 			elasped := time.Since(start).Seconds()
-			client.Histogram("add_task_exec_time.histogram",elasped, []string{"environment:dev"}, 1)
-			
+			client.Histogram("add_task_exec_time.histogram", elasped, []string{"environment:dev"}, 1)
+
 			//collect data on get tasks
 			start = time.Now()
 			getTasks(true)
 			elasped = time.Since(start).Seconds()
-			client.Histogram("get_tasks_exec_time.histogram",elasped, []string{"environment:dev"}, 1)
-			
+			client.Histogram("get_tasks_exec_time.histogram", elasped, []string{"environment:dev"}, 1)
+
+			//collect data on time to complete tasks
+			start = time.Now()
+			completeTask(1) //completes all tasks with id of 1
+			elasped = time.Since(start).Seconds()
+			client.Histogram("get_tasks_exec_time.histogram", elasped, []string{"environment:dev"}, 1)
+
 		}
-		time.Sleep(120*time.Second)
+		time.Sleep(120 * time.Second)
 	}
 }
